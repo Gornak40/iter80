85fb07022036e6f377cd4ff66b63bb04f81a61e2
Gornak40 <s-kozelsk@yandex.ru>
2025-07-12 15:18:14 +0300

    fix: remove labels; rename Tokenizer

--- src/root.zig
+++ src/root.zig
@@ -15,10 +15,6 @@ const Token = struct {
             .{ .type = .@"a=", .meta = s[0 .. s.len - 1] }
         else if (s[0] == '=')
             .{ .type = .@"=a", .meta = s[1..] }
-        else if (s[s.len - 1] == ':')
-            .{ .type = .@"a:", .meta = s[0 .. s.len - 1] }
-        else if (s[0] == ':')
-            .{ .type = .@":a", .meta = s[1..] }
         else if (s.len >= 2 and s[0] == '`' and s[s.len - 1] == '`')
             .{ .type = .assembly, .meta = s[1 .. s.len - 1] }
         else
@@ -31,23 +27,19 @@ const Token = struct {
         @"..",
         @"a=",
         @"=a",
-        @"a:",
-        @":a",
         assembly,
         atom,
     };
 };
 
-const Tokenizer = struct {
+const RawTokenizer = struct {
     buf: []const u8,
 
-    fn init(s: []const u8) Tokenizer {
-        return .{
-            .buf = s,
-        };
+    fn init(s: []const u8) RawTokenizer {
+        return .{ .buf = s };
     }
 
-    fn next(self: *Tokenizer) ?[]const u8 {
+    fn next(self: *RawTokenizer) ?[]const u8 {
         return while (self.buf.len > 0) {
             var i: usize = 0;
             defer self.buf = self.buf[@min(i + 1, self.buf.len)..];
@@ -66,9 +58,10 @@ const Tokenizer = struct {
     }
 };
 
+/// Lifetime of `s` must be not less than lifetime of function result.
 pub fn tokenizeFromSlice(alloc: std.mem.Allocator, s: []const u8) ![]Token {
     var list = std.ArrayList(Token).init(alloc);
-    var iter = Tokenizer.init(s);
+    var iter = RawTokenizer.init(s);
     while (iter.next()) |raw| {
         try list.append(Token.init(raw));
     }
@@ -81,6 +74,7 @@ test "tokenizer" {
         \\a=    a
         \\      b
         \\`add a0, =a, a0`
+        \\  =a
         \\..
     ;
     const alloc = std.testing.allocator;
@@ -96,6 +90,7 @@ test "tokenizer" {
         .{ .type = .atom, .meta = "a" },
         .{ .type = .atom, .meta = "b" },
         .{ .type = .assembly, .meta = "add a0, =a, a0" },
+        .{ .type = .@"=a", .meta = "a" },
         .{ .type = .@".." },
     }, tokens);
 }
