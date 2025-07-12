5a37d417b230a64db81b0a868d5a1382f8282e52
Gornak40 <s-kozelsk@yandex.ru>
2025-07-12 15:08:28 +0300

    feat: add demo tokenizer

--- src/main.zig
+++ src/main.zig
@@ -2,4 +2,22 @@ const std = @import("std");
 
 const iter = @import("iter");
 
-pub fn main() !void {}
+pub fn main() !void {
+    var gpa: std.heap.GeneralPurposeAllocator(.{}) = .{};
+    const alloc = gpa.allocator();
+
+    const args = try std.process.argsAlloc(alloc);
+    if (args.len != 2) {
+        return error.UsageError; // TODO: more verbose
+    }
+
+    var file = try std.fs.cwd().openFile(args[1], .{});
+    defer file.close();
+
+    const source = try file.readToEndAlloc(alloc, std.math.maxInt(u32));
+    defer alloc.free(source);
+
+    const tokens = try iter.tokenizeFromSlice(alloc, source);
+    defer alloc.free(tokens);
+    std.debug.print("{any}\n", .{tokens});
+}
--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,101 @@
 const std = @import("std");
-const testing = std.testing;
 
-test "basic add functionality" {
-    try testing.expectEqual(3, 1 + 2);
+const Token = struct {
+    type: Type,
+    meta: ?[]const u8 = null,
+
+    fn init(s: []const u8) Token {
+        return if (std.mem.startsWith(u8, s, "__"))
+            .{ .type = .__a, .meta = s[2..] }
+        else if (std.mem.eql(u8, s, "::"))
+            .{ .type = .@"::" }
+        else if (std.mem.eql(u8, s, ".."))
+            .{ .type = .@".." }
+        else if (s[s.len - 1] == '=')
+            .{ .type = .@"a=", .meta = s[0 .. s.len - 1] }
+        else if (s[0] == '=')
+            .{ .type = .@"=a", .meta = s[1..] }
+        else if (s[s.len - 1] == ':')
+            .{ .type = .@"a:", .meta = s[0 .. s.len - 1] }
+        else if (s[0] == ':')
+            .{ .type = .@":a", .meta = s[1..] }
+        else if (s.len >= 2 and s[0] == '`' and s[s.len - 1] == '`')
+            .{ .type = .assembly, .meta = s[1 .. s.len - 1] }
+        else
+            .{ .type = .atom, .meta = s };
+    }
+
+    const Type = enum {
+        __a,
+        @"::",
+        @"..",
+        @"a=",
+        @"=a",
+        @"a:",
+        @":a",
+        assembly,
+        atom,
+    };
+};
+
+const Tokenizer = struct {
+    buf: []const u8,
+
+    fn init(s: []const u8) Tokenizer {
+        return .{
+            .buf = s,
+        };
+    }
+
+    fn next(self: *Tokenizer) ?[]const u8 {
+        return while (self.buf.len > 0) {
+            var i: usize = 0;
+            defer self.buf = self.buf[@min(i + 1, self.buf.len)..];
+            if (self.buf[0] == '`') {
+                if (std.mem.indexOfScalarPos(u8, self.buf, 1, '`')) |r| {
+                    i = r;
+                    break self.buf[0 .. r + 1];
+                }
+            }
+            while (i < self.buf.len) : (i += 1) {
+                if (std.ascii.isWhitespace(self.buf[i]))
+                    break;
+            }
+            if (i != 0) break self.buf[0..i];
+        } else null;
+    }
+};
+
+pub fn tokenizeFromSlice(alloc: std.mem.Allocator, s: []const u8) ![]Token {
+    var list = std.ArrayList(Token).init(alloc);
+    var iter = Tokenizer.init(s);
+    while (iter.next()) |raw| {
+        try list.append(Token.init(raw));
+    }
+    return list.toOwnedSlice();
+}
+
+test "tokenizer" {
+    const prog =
+        \\__foo a b ::
+        \\a=    a
+        \\      b
+        \\`add a0, =a, a0`
+        \\..
+    ;
+    const alloc = std.testing.allocator;
+    const tokens = try tokenizeFromSlice(alloc, prog);
+    defer alloc.free(tokens);
+
+    try std.testing.expectEqualDeep(&[_]Token{
+        .{ .type = .__a, .meta = "foo" },
+        .{ .type = .atom, .meta = "a" },
+        .{ .type = .atom, .meta = "b" },
+        .{ .type = .@"::" },
+        .{ .type = .@"a=", .meta = "a" },
+        .{ .type = .atom, .meta = "a" },
+        .{ .type = .atom, .meta = "b" },
+        .{ .type = .assembly, .meta = "add a0, =a, a0" },
+        .{ .type = .@".." },
+    }, tokens);
 }
