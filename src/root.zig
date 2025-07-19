e1e628f7d2b0885bfe4040c3acc8109a8beb7c44
Gornak40 <s-kozelsk@yandex.ru>
2025-07-19 12:27:32 +0300

    feat: ast intro

--- src/root.zig
+++ src/root.zig
@@ -16,7 +16,7 @@ const Token = struct {
         else if (s[0] == '=')
             .{ .type = .@"=a", .meta = s[1..] }
         else if (s.len >= 2 and s[0] == '`' and s[s.len - 1] == '`')
-            .{ .type = .assembly, .meta = s[1 .. s.len - 1] }
+            .{ .type = .@"inline", .meta = s[1 .. s.len - 1] }
         else
             .{ .type = .atom, .meta = s };
     }
@@ -27,7 +27,7 @@ const Token = struct {
         @"..",
         @"a=",
         @"=a",
-        assembly,
+        @"inline",
         atom,
     };
 };
@@ -95,22 +95,111 @@ test "tokenizer" {
     }, tokens);
 }
 
+pub const ParseError = error{
+    EOF,
+    UnexpectedType,
+    EmptyMeta,
+};
+
+const TokenReader = struct {
+    tokens: []const Token,
+
+    fn readExpected(self: *TokenReader, expected: Token.Type) !Token {
+        const tok = try self.peek();
+        if (tok.type != expected) return ParseError.UnexpectedType;
+        defer self.tokens = self.tokens[1..];
+        return tok;
+    }
+
+    fn readMetaExpected(self: *TokenReader, expected: Token.Type) ![]const u8 {
+        const tok = try self.readExpected(expected);
+        return tok.meta orelse ParseError.EmptyMeta;
+    }
+
+    fn peek(self: TokenReader) !Token {
+        return if (self.isEmpty()) ParseError.EOF else self.tokens[0];
+    }
+
+    fn peekType(self: TokenReader) !Token.Type {
+        return (try self.peek()).type;
+    }
+
+    fn isEmpty(self: TokenReader) bool {
+        return self.tokens.len == 0;
+    }
+};
+
+pub fn buildASTLeaky(alloc: std.mem.Allocator, tokens: []const Token) !*const NodeRoot {
+    const r: TokenReader = .{ .tokens = tokens };
+    return NodeRoot.init(alloc, r);
+}
+
 const NodeRoot = struct {
     suncs: []*const NodeSuncDecl,
+
+    fn init(alloc: std.mem.Allocator, r: *TokenReader) !*const NodeRoot {
+        const node = try alloc.create(@This());
+        var suncs = std.ArrayList(*const NodeSuncDecl).init(alloc);
+        while (!r.isEmpty()) {
+            const sunc = try NodeSuncDecl.init(alloc, r);
+            try suncs.append(sunc);
+        }
+
+        node.* = .{ .suncs = try suncs.toOwnedSlice() };
+        return node;
+    }
 };
 
 const NodeSuncDecl = struct {
     name: []const u8,
     args: []*const NodeArgDecl,
     body: []INodeStmt,
+
+    fn init(alloc: std.mem.Allocator, r: *TokenReader) !*const NodeSuncDecl {
+        const node = try alloc.create(@This());
+        const name = try r.readMetaExpected(.__a);
+
+        var args = std.ArrayList(*const NodeArgDecl).init(alloc);
+        while (try r.peekType() != .@"::") {
+            const arg = try NodeArgDecl.init(alloc, r);
+            try args.append(arg);
+        }
+        _ = r.readExpected(.@"::");
+
+        var body = std.ArrayList(INodeStmt).init(alloc);
+        while (try r.peekType() != .@"..") {
+            const arg = try INodeStmt.init(alloc, r);
+            try body.append(arg);
+        }
+        _ = r.readExpected(.@"..");
+
+        node.* = .{ .name = name, .args = try args.toOwnedSlice(), .body = try body.toOwnedSlice() };
+        return node;
+    }
 };
 
 const NodeArgDecl = struct {
     name: []const u8,
+
+    fn init(alloc: std.mem.Allocator, r: *TokenReader) !*const NodeArgDecl {
+        const node = try alloc.create(@This());
+        const name = try r.readMetaExpected(.atom);
+
+        node.* = .{ .name = name };
+        return node;
+    }
 };
 
 const NodeArgPlace = struct {
     name: []const u8,
+
+    fn init(alloc: std.mem.Allocator, r: *TokenReader) !*const NodeArgPlace {
+        const node = try alloc.create(@This());
+        const name = try r.readMetaExpected(.atom);
+
+        node.* = .{ .name = name };
+        return node;
+    }
 };
 
 const NodeTagPlace = struct {
@@ -131,10 +220,15 @@ const NodeTaggedExpr = struct {
     expr: INodeExpr,
 };
 
+const NodeInlineExpr = struct {
+    @"inline": *const NodeInline,
+    expr: INodeExpr,
+};
+
 const INodeStmt = union(enum) {
     expr: INodeExpr,
     tagged_expr: NodeTaggedExpr,
-    assembly: *const NodeAssembly,
+    inline_expr: NodeInlineExpr,
 };
 
 const INodeExpr = union(enum) {
@@ -143,6 +237,21 @@ const INodeExpr = union(enum) {
     sunc_place: *const NodeSuncPlace,
 };
 
-const NodeAssembly = struct {
-    // TODO
+const NodeInline = struct {
+    tmpl: []const u8,
+    opts: []INodeTmplOpt,
+    next_inline: ?*const NodeInline,
+};
+
+const NodeTmplUId = struct {
+    name: []const u8,
+};
+
+const NodeTmplTag = struct {
+    name: []const u8,
+};
+
+const INodeTmplOpt = union(enum) {
+    tmpl_uid: *const NodeTmplUId,
+    tmpl_tag: *const NodeTmplTag,
 };
